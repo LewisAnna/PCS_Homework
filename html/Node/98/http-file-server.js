0037db91755ae20c5e61c360f518759ec30cf852
Anna <chanalewis811@gmail.com>
2026-02-22 17:02:02 -0500

    new

---
+++ html/Node/98/http-file-server.js
@@ -0,0 +1,7 @@
+     const http = require('http')
+     const server = http.createServer(function (req, res) {
+       fs.createReadStream(){
+        src.pipe(dst)
+       }
+     })
+     server.listen(8000)
---
+++ html/Node/98/http-uppercaserer.js
@@ -0,0 +1,4 @@
+     const map = require('through2-map')
+     inStream.pipe(map(function (chunk) {
+       return chunk.toString().split('').reverse().join('')
+     })).pipe(outStream)
